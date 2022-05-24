194514c19037e5ce9d9e07caeb6654aca510af23
ghostracoon <gusalvador1028@gmail.com>
2022-05-24 17:55:44 +0000

    mudança na tabela plataforma

--- src/main/java/application/models/Plataforma.java
+++ src/main/java/application/models/Plataforma.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import java.util.Set;
@@ -32,4 +32,15 @@ public class Plataforma {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    @ManyToMany(mappedBy = "plataforma")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public Set<Jogo> getJogos() {
+        return jogos;
+    }
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
 }
